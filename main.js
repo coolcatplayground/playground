6d6a7f37a4f8784bbd4763b8d5171bfde8168502
coolcatplayground <chacuttayapongwiluk@gmail.com>
2025-10-24 15:19:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -110,21 +110,21 @@ langSwitcher.addEventListener("change", (e) => {
       subtitle: "Elegant musings on hobbies and daily life",
       footer: "© 2025 CoolCat's Hobby & Daily Diary. All rights reserved.",
       cta: "Which language do you prefer to talk with CC?",
-      nav: ["Top Page", "Adventure Archive", "Omnious Library", "Image Lab"],
+      nav: ["Top Page", "Adventure Archive", "Omnious Library", "Image Lab", "Google"],
     },
     ja: {
       title: "クールキャットの趣味と日々の記録",
       subtitle: "趣味と日常生活に関する優雅な思索",
       footer: "© 2025 クールキャットの趣味と日々の記録. 全著作権所有.",
       cta: "CCとどの言語で話したいですか？",
-      nav: ["トップページ", "冒険アーカイブ", "オミナス・ライブラリー", "イメージ・ラボ"],
+      nav: ["トップページ", "冒険アーカイブ", "不吉な図書館", "画像ラボ", "グーグル"],
     },
     th: {
       title: "บันทึกประจำวันและงานอดิเรกของ CoolCat",
       subtitle: "แรงบันดาลใจจากงานอดิเรกและชีวิตประจำวัน",
       footer: "© 2025 บันทึกประจำวันและงานอดิเรกของ CoolCat สงวนลิขสิทธิ์ทั้งหมด",
       cta: "คุณต้องการคุยกับ CC ด้วยภาษาใด?",
-      nav: ["หน้าแรก", "บันทึกการผจญภัย", "ห้องสมุดลึกลับ", "ห้องภาพศิลป์"],
+      nav: ["หน้าแรก", "บันทึกการผจญภัย", "ห้องสมุดลึกลับ", "ห้องภาพศิลป์", "กูเกิล"],
     },
   };
 
@@ -139,7 +139,7 @@ langSwitcher.addEventListener("change", (e) => {
   // Navigation
   const navLabels = document.querySelectorAll("#main-nav a");
   navLabels.forEach((link, i) => {
-    link.textContent = t.nav[i];
+    if (t.nav[i]) link.textContent = t.nav[i];
   });
 }
 
